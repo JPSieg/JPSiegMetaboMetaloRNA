b7668d977497ba72dfc8c90772143e3c06f71898
JPSieg <jpsieg225@gmail.com>
2022-01-12 16:03:33 -0500

    Add new fluorescence data

--- R/Figure_3_script.R
+++ R/Figure_3_script.R
@@ -12,8 +12,6 @@ correction = function(x){ #function to apply a temp correction
 paste("Figures/Figure_3/Fluorescence_data", list.files("Figures/Figure_3/Fluorescence_data"), sep = "/")
 paste("Figures/Figure_3/Index_files", list.files("Figures/Figure_3/Index_files"), sep = "/")
 
-?AB.qPCR.export.to.MeltR.csv
-
 #js5057 NTP-Mg F
 
 df = AB.qPCR.export.to.MeltR.csv("Figures/Figure_3/Fluorescence_data/js5057_Helix_F_and_G_data.txt",
@@ -133,6 +131,69 @@ df = AB.qPCR.export.to.MeltR.csv("Figures/Figure_3/Fluorescence_data/js5060_Heli
 df = df %>% filter(Reading == 1)
 
 plot(df$B, df$Emission) #looks good
+
+#js6002 NTP-Mg F
+
+list.files("Figures/Figure_3/Index_files/")
+
+df = AB.qPCR.export.to.MeltR.csv("Figures/Figure_3/Fluorescence_data/js6004_Helix_F_and_G_data.txt",
+                                 "Figures/Figure_3/Index_files/js6004_Helix_F_index.csv",
+                                 "Figures/Figure_3/Fluorescence_data/js6004_Helix_F_formatted.csv",
+                                 correction,
+                                 Scale = 100000)
+
+df = df %>% filter(Reading == 1)
+
+plot(df$B, df$Emission) #looks good
+
+#js5057 NTP-Mg G
+
+df = AB.qPCR.export.to.MeltR.csv("Figures/Figure_3/Fluorescence_data/js6004_Helix_F_and_G_data.txt",
+                                 "Figures/Figure_3/Index_files/js6004_Helix_G_index.csv",
+                                 "Figures/Figure_3/Fluorescence_data/js6004_Helix_G_formatted.csv",
+                                 correction,
+                                 Scale = 100000)
+
+df = df %>% filter(Reading == 1)
+
+plot(df$B, df$Emission) #looks good
+
+#js5057 NTP-Mg H
+
+df = AB.qPCR.export.to.MeltR.csv("Figures/Figure_3/Fluorescence_data/js6004_Helix_H_and_I_data.txt",
+                                 "Figures/Figure_3/Index_files/js6004_Helix_H_index.csv",
+                                 "Figures/Figure_3/Fluorescence_data/js6004_Helix_H_formatted.csv",
+                                 correction,
+                                 Scale = 100000)
+
+df = df %>% filter(Reading == 1)
+
+plot(df$B, df$Emission) #looks good
+
+#js5057 NTP-Mg I
+
+df = AB.qPCR.export.to.MeltR.csv("Figures/Figure_3/Fluorescence_data/js6004_Helix_H_and_I_data.txt",
+                                 "Figures/Figure_3/Index_files/js6004_Helix_I_index.csv",
+                                 "Figures/Figure_3/Fluorescence_data/js6004_Helix_I_formatted.csv",
+                                 correction,
+                                 Scale = 100000)
+
+df = df %>% filter(Reading == 1)
+
+plot(df$B, df$Emission) #looks good
+
+#js5057 NTP-Mg J
+
+df = AB.qPCR.export.to.MeltR.csv("Figures/Figure_3/Fluorescence_data/js6004_Helix_J_data.txt",
+                                 "Figures/Figure_3/Index_files/js6004_Helix_J_index.csv",
+                                 "Figures/Figure_3/Fluorescence_data/js6004_Helix_J_formatted.csv",
+                                 correction,
+                                 Scale = 100000)
+
+df = df %>% filter(Reading == 1)
+
+plot(df$B, df$Emission) #looks good
+
 
 ####Fit data####
 
@@ -303,6 +364,87 @@ list.fit[[10]]$Tms$Condition = "2mMMg-free"
 list.fit[[10]]$VantHoff$Helix = "J"
 list.fit[[10]]$VantHoff$Condition = "2mMMg-free"
 
+#NTPCM Helix F
+
+list.df[[11]] = read.csv("Figures/Figure_3/Fluorescence_data/js6004_Helix_F_formatted.csv")
+
+
+list.fit[[11]] = meltR.F(list.df[[11]],
+                        file_prefix = "Figures/Figure_3/Fluorescence_data/23mMNTPCM_Helix_F",
+                        Save_results = "all",
+                        B.conc.Tm = 175)
+
+list.fit[[11]]$Tms$Helix = "F"
+list.fit[[11]]$Tms$Condition = "25mMMg-NTPs"
+list.fit[[11]]$VantHoff$Helix = "F"
+list.fit[[11]]$VantHoff$Condition = "25mMMg-NTPs"
+
+#NTPCM Helix G
+
+paste("Figures/Figure_3/Fluorescence_data", list.files("Figures/Figure_3/Fluorescence_data"), sep = "/")
+
+list.df[[12]] = read.csv("Figures/Figure_3/Fluorescence_data/js6004_Helix_G_formatted.csv")
+
+list.fit[[12]] = meltR.F(list.df[[12]],
+                        file_prefix = "Figures/Figure_3/Fluorescence_data/25mMNTPCM_Helix_G",
+                        Save_results = "all",
+                        B.conc.Tm = 250)
+
+list.fit[[12]]$Tms$Helix = "G"
+list.fit[[12]]$Tms$Condition = "25mMMg-NTPs"
+list.fit[[12]]$VantHoff$Helix = "G"
+list.fit[[12]]$VantHoff$Condition = "25mMMg-NTPs"
+
+#NTPCM Helix H
+
+paste("Figures/Figure_3/Fluorescence_data", list.files("Figures/Figure_3/Fluorescence_data"), sep = "/")
+
+list.df[[13]] = read.csv("Figures/Figure_3/Fluorescence_data/js6004_Helix_H_formatted.csv")
+
+list.fit[[13]] = meltR.F(list.df[[13]],
+                        file_prefix = "Figures/Figure_3/Fluorescence_data/25mMNTPCM_Helix_H",
+                        Save_results = "all",
+                        B.conc.Tm = 300)
+
+list.fit[[13]]$Tms$Helix = "H"
+list.fit[[13]]$Tms$Condition = "25mMMg-NTPs"
+list.fit[[13]]$VantHoff$Helix = "H"
+list.fit[[13]]$VantHoff$Condition = "25mMMg-NTPs"
+
+#NTPCM Helix I
+
+paste("Figures/Figure_3/Fluorescence_data", list.files("Figures/Figure_3/Fluorescence_data"), sep = "/")
+
+list.df[[14]] = read.csv("Figures/Figure_3/Fluorescence_data/js6004_Helix_I_formatted.csv")
+
+list.fit[[14]] = meltR.F(list.df[[14]],
+                        file_prefix = "Figures/Figure_3/Fluorescence_data/25mMNTPCM_Helix_I",
+                        Save_results = "all",
+                        B.conc.Tm = 200)
+
+list.fit[[14]]$Tms$Helix = "I"
+list.fit[[14]]$Tms$Condition = "25mMMg-NTPs"
+list.fit[[14]]$VantHoff$Helix = "I"
+list.fit[[14]]$VantHoff$Condition = "25mMMg-NTPs"
+
+#NTPCM Helix J
+
+paste("Figures/Figure_3/Fluorescence_data", list.files("Figures/Figure_3/Fluorescence_data"), sep = "/")
+
+list.df[[15]] = read.csv("Figures/Figure_3/Fluorescence_data/js6004_Helix_J_formatted.csv")
+
+list.fit[[15]] = meltR.F(list.df[[15]],
+                        file_prefix = "Figures/Figure_3/Fluorescence_data/25mMNTPCM_Helix_J",
+                        Save_results = "all",
+                        K_error = c(0.62, 0.62),
+                        B.conc.Tm = 200)
+
+list.fit[[15]]$Tms$Helix = "J"
+list.fit[[15]]$Tms$Condition = "16mMMg-NTPs"
+list.fit[[15]]$VantHoff$Helix = "J"
+list.fit[[15]]$VantHoff$Condition = "16mMMg-NTPs"
+
+
 ####Look at Tms####
 
 df.Tms = bind_rows(list.fit[[1]]$Tms,
@@ -314,7 +456,12 @@ df.Tms = bind_rows(list.fit[[1]]$Tms,
                    list.fit[[7]]$Tms,
                    list.fit[[8]]$Tms,
                    list.fit[[9]]$Tms,
-                   list.fit[[10]]$Tms)
+                   list.fit[[10]]$Tms,
+                   list.fit[[11]]$Tms,
+                   list.fit[[12]]$Tms,
+                   list.fit[[13]]$Tms,
+                   list.fit[[14]]$Tms,
+                   list.fit[[15]]$Tms)
 
 head(df.Tms)
 
@@ -322,31 +469,3 @@ ggplot(df.Tms, aes(x = Condition, y = Tm, color = Helix)) +
   geom_beeswarm() +
   theme_classic()
 
-####Look at free energies####
-
-
-df.Energy = bind_rows(list.fit[[1]]$VantHoff,
-                      list.fit[[2]]$VantHoff,
-                      list.fit[[3]]$VantHoff,
-                      list.fit[[4]]$VantHoff,
-                      list.fit[[5]]$VantHoff,
-                      list.fit[[6]]$VantHoff,
-                      list.fit[[7]]$VantHoff,
-                      list.fit[[8]]$VantHoff,
-                      list.fit[[9]]$VantHoff,
-                      list.fit[[10]]$VantHoff)
-
-df.Energy
-
-df.Energy = df.Energy %>%
-  filter(Method == "2 Global fit") %>%
-  arrange(Helix)
-
-ggplot(df.Energy, aes(x = Helix, y = G, ymin = G - SE.G, ymax = G + SE.G, color = Condition))+
-  geom_pointrange()
-
-ggplot(df.Energy, aes(x = Helix, y = H, ymin = H - SE.H, ymax = H + SE.H, color = Condition))+
-  geom_pointrange()
-
-ggplot(df.Energy, aes(x = Helix, y = S, ymin = S - SE.S, ymax = S + SE.S, color = Condition))+
-  geom_pointrange()
